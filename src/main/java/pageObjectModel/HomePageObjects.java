d49a2b1192da93927400194437bc20cd031630de
piyushpp4997 <piyushpund4@gmail.com>
2023-09-27 11:59:52 +0530

    added homepage functionality

---
+++ src/main/java/pageObjectModel/HomePageObjects.java
@@ -0,0 +1,9 @@
+package pageObjectModel;
+
+import org.openqa.selenium.By;
+
+public class HomePageObjects {
+
+	private By login = By.xpath("//input[@id='Login']");
+	By Employees = By.xpath("//select[@name='CompanyEmployees']");
+}
